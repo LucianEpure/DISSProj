ae8ee98393eb94cc65527425a69b94ec8c0bd99c
Lucian <Lucian>
2020-05-11 18:51:42 +0300

    initial commit

---
+++ src/test/java/com/example/DissPr/DissPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DissPr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
